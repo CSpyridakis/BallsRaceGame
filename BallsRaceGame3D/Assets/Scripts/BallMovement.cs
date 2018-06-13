05f851021c0d70b51c2c50ab6d611deb82c856ea
Spyridakis Christos <spyridakischristos@gmail.com>
2018-06-13 22:51:05 +0300

    1) (Hex created 1,75 | dis | )  2) Floor, obs, boos, build added

---
+++ BallsRaceGame3D/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		
+		Vector3 d =new Vector3(h,0,v);
+		
+		gameObject.transform.Translate(d.normalized*Time.deltaTime* speed);
+	}
+}
