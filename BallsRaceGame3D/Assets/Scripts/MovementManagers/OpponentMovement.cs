23b27a6a6c0257ca7e63c3755f60ecf6f998d498
Spyridakis Christos <spyridakischristos@gmail.com>
2018-06-18 06:40:31 +0300

    Opponent Movement optimized

--- BallsRaceGame3D/Assets/Scripts/MovementManagers/OpponentMovement.cs
+++ BallsRaceGame3D/Assets/Scripts/MovementManagers/OpponentMovement.cs
@@ -11,17 +11,24 @@ using Random = UnityEngine.Random;
 public class OpponentMovement : MonoBehaviour {
 
 	public float currentSpeed=11.0f;
-	public float speedIncreaseRate = 1.0f;
+	public float speedIncreaseRate = 2.0f;
 	public float speedUpLimit = 70.0f;
 	public float leftRightspeed=20.0f;
-	public float checkDistance = 20.0f;
+	public float checkDistance = 50.0f;
 	public float offsetCheck = 0.53f;
 	public float lrOffset = 4f;
 	
+	private float leftFloorPoss;
+	private float rightFloorPoss;
 	private Vector3 moveV;
 	private int lastob =0;
+	private int ID;
 	void Start ()
 	{
+		ID = Random.Range(0, 10000);
+		GameObject fl = GameObject.FindGameObjectWithTag("Floor");
+		leftFloorPoss = -0.1f;//fl.transform.position.x-lrOffset;
+		rightFloorPoss = 7.1f;//fl.transform.position.x + lrOffset;
 		//Debug.Log("Begin Ball Movement: OK");
 		currentSpeed=Random.Range(7f,16f);
 	}
@@ -67,12 +74,6 @@ public class OpponentMovement : MonoBehaviour {
 	/*
 	 * @brief Find Obstacles position and move opponent left or right in order to avoid them  
 	 */
-	/*
-	 * @brief Find opponents speed based on his distance until obstacle
-	 *
-	 * @param (float) distance , Distance of collision
-	 * @param (float) x, current players position
-	 */
 	public float leftRightMovement()
 	{
 		//Raycasts
@@ -92,27 +93,22 @@ public class OpponentMovement : MonoBehaviour {
 		//Debug.DrawRay(transform.position, transform.forward,Color.green);
 		//Debug.DrawRay(rightCheck, transform.forward,Color.blue);
 		
-		//Out of scene Case
-		GameObject fl = GameObject.FindGameObjectWithTag("Floor");
-		float leftFloorPoss=fl.transform.position.x-lrOffset;
-		float rightFloorPoss = fl.transform.position.x + lrOffset;
-		
 		//Forward Collision
-		if ((hitForward.distance<hitLeft.distance) && (hitForward.distance<hitRight.distance) && (hitForward.distance<checkDistance))
+		if ((hitForward.distance<hitLeft.distance) && (hitForward.distance<hitRight.distance) && (hitForward.distance<checkDistance+currentSpeed) && hitForward.collider.CompareTag("Obstacle"))
 		{
-			findDirection();
+			findDirection(0);
 			return lastob*leftRightSpeed(hitForward.distance, transform.position.x);
 		}
 		//Left Collision
-		else if ((hitLeft.distance < hitForward.distance) && (hitLeft.distance < hitRight.distance) && (hitLeft.distance < checkDistance))
+		else if ((hitLeft.distance < hitForward.distance) && (hitLeft.distance < hitRight.distance) && (hitLeft.distance < checkDistance+currentSpeed)&& hitLeft.collider.CompareTag("Obstacle"))
 		{
-			findDirection();
+			findDirection(-1);
 			return lastob*leftRightSpeed(hitLeft.distance, transform.position.x);
 		}
 		//Right Collision
-		else if ((hitRight.distance < hitForward.distance) && (hitRight.distance < hitLeft.distance) && (hitRight.distance < checkDistance))
+		else if ((hitRight.distance < hitForward.distance) && (hitRight.distance < hitLeft.distance) && (hitRight.distance < checkDistance+currentSpeed)&& hitRight.collider.CompareTag("Obstacle"))
 		{
-			findDirection();
+			findDirection(1);
 			return lastob*leftRightSpeed(hitRight.distance, transform.position.x);
 		}
 		//No Collision
@@ -120,15 +116,18 @@ public class OpponentMovement : MonoBehaviour {
 		{
 			lastob = 0;
 		}
-		
 		return 0.0f;
 	}
 
+	/*
+	 * @brief Find opponents speed based on his distance until obstacle
+	 *
+	 * @param (float) distance , Distance of collision
+	 * @param (float) x, current players position
+	 */
 	private float leftRightSpeed(float distance, float x)
 	{
-		GameObject fl = GameObject.FindGameObjectWithTag("Floor");
-		float leftFloorPoss=fl.transform.position.x-lrOffset;
-		float rightFloorPoss = fl.transform.position.x + lrOffset;
+
 		//Opponent out in scene
 		if (x > leftFloorPoss && x < rightFloorPoss)
 		{
@@ -141,7 +140,7 @@ public class OpponentMovement : MonoBehaviour {
 				return leftRightspeed;
 			}
 
-			return leftRightspeed / distance;
+			return leftRightspeed / distance + currentSpeed %10;
 		}
 		//Opponent out off scene
 		else
@@ -152,20 +151,32 @@ public class OpponentMovement : MonoBehaviour {
 		
 	/*
 	 * @brief Find if opponent has to move left or right
+	 *
+	 * @param collisionFrom, position from where collision is going to happen
 	 */
-	private void findDirection()
+	private void findDirection(int collisionFrom)
 	{
-		GameObject fl = GameObject.FindGameObjectWithTag("Floor");
-		float leftFloorPoss=fl.transform.position.x-lrOffset;
-		float rightFloorPoss = fl.transform.position.x + lrOffset;
-
 		float distanceToRight=Math.Abs(transform.position.x-leftFloorPoss);
 		float distanceToLeft=Math.Abs(transform.position.x-rightFloorPoss);
 		
 		//Middle Case
-		if (Math.Abs(distanceToRight-distanceToLeft)<0.5f && lastob==0)
+		if (Math.Abs(distanceToRight-distanceToLeft)<0.1f && lastob==0)
 		{
-			lastob = Random.Range(0, 1) == 0 ? -1 : 1;
+			//If collision from left move right
+			if (collisionFrom==-1)
+			{
+				lastob = 1;
+			}
+			//If collision from right move left
+			else if(collisionFrom==1)
+			{
+				lastob = -1;
+			}
+			//If collision from middle random select
+			else
+			{
+				lastob = Random.Range(0, 1) == 0 ? -1 : 1;
+			}
 		}
 		//Move right case
 		else if(distanceToLeft<distanceToRight && lastob==0)
@@ -177,7 +188,6 @@ public class OpponentMovement : MonoBehaviour {
 		{
 			lastob = -1;
 		}
-		
 		//Fix Direction in case of Error
 		if(transform.position.x<leftFloorPoss)
 			lastob = 1;
